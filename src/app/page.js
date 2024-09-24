7099b06fa43e88ea405cdaef42070eb4eaeb2cf4
keval3 <keval@gumlet.com>
2024-09-24 16:11:52 +0530

    change gumelt playback url

--- src/app/page.js
+++ src/app/page.js
@@ -2,5 +2,5 @@ import Video from 'next-video';
 // import awesomeVideo from 'https://play-v2.gumlet.io/embed/6685517070a16ee4eb1f321c';
  
 export default function Home() {
-  return <Video src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" />;
+  return <Video src="https://stream.smartplayer.ai/600e2eccc1be82e7b5b29467/64628b7110f328a292ade85e/main.mp4" />;
 }
