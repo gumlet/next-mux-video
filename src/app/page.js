e044bc343f87b15c235d17a517e0479b29f0ff4d
keval3 <keval@gumlet.com>
2024-09-24 16:17:04 +0530

    hls url

--- src/app/page.js
+++ src/app/page.js
@@ -2,5 +2,5 @@ import Video from 'next-video';
 // import awesomeVideo from 'https://play-v2.gumlet.io/embed/6685517070a16ee4eb1f321c';
  
 export default function Home() {
-  return <Video src="https://stream.smartplayer.ai/600e2eccc1be82e7b5b29467/64628b7110f328a292ade85e/main.mp4" />;
+  return <Video src="https://video.gumlet.io/5f462c1561cf8a766464ffc4/66ea8e9547b062984848f927/main.m3u8" />;
 }
